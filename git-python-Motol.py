47283443037c0533b656ca1029ee05474afd9e22
MotolRochelle <motolrochelle5@gmail.com>
2022-03-28 11:22:37 +0800

    My Educational Background added

--- git-python-Motol.py
+++ git-python-Motol.py
@@ -1,3 +1,6 @@
 print ('Rochelle Nakario Motol');
 print ('Sihi Buenavista Marinduque');
 print ('March 4, 2002');
+print ('Marinduque State College');
+print ('Bachelor of Science in Information Technology');
+print ('2-F');
