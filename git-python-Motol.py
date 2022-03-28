4ad4904a60b9b8f680c52d1c8803fc3fee5e4641
MotolRochelle <motolrochelle5@gmail.com>
2022-03-28 11:26:15 +0800

    My Hobbies added

--- git-python-Motol.py
+++ git-python-Motol.py
@@ -4,3 +4,8 @@ print ('March 4, 2002');
 print ('Marinduque State College');
 print ('Bachelor of Science in Information Technology');
 print ('2-F');
+print ('Editing')
+print ('Designing');
+print ('Reading Books and Wattpad');
+print ('Watching Anime and Hollywood movies');
+print ('Travel');
